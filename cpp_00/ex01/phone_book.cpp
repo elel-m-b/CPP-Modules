03328f2d3e606e268b0837919a69f164080c3481
El Hassane El M'Barki <mbkhassan10@gmail.com>
2025-09-02 21:33:11 +0100

    megaphon start

--- cpp_00/ex01/phone_book.cpp
+++ cpp_00/ex01/phone_book.cpp
@@ -4,16 +4,36 @@
 class      PhoneBook
 {
     public:
-
+        std::string     first_name;
+        std::string     last_name;
+        std::string     nickname;
+        std::string     phone_number;
+    // public:
+    //     void        fill_contact(std::string info)
+    //     {
+    //         first_name = info;
+    //     }
 };
 
 class      Contact
 {
     public:
+        PhoneBook       call;
     
 };
 
-int main()
+int main(int        ac, char        **av)
 {
-
+    if (ac == 1)
+        return (0);
+    std::string     command = av[1];
+    Contact     p;
+    while (1)
+    {
+        if (command == "ADD")
+        {
+            std::cout << "Enter your contact\n";
+            std::cin >> p.call.first_name;
+        }
+    }
 }
