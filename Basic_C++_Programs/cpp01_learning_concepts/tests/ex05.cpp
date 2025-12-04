d5cc303dc5a1d8a9040717ca8e20efb2d0c0483f
El Hassane El M'Barki <mbkhassan10@gmail.com>
2025-12-04 15:49:39 +0100

    feat:: add ex05

--- Basic_C++_Programs/cpp01_learning_concepts/tests/ex05.cpp
+++ Basic_C++_Programs/cpp01_learning_concepts/tests/ex05.cpp
@@ -1,27 +1,55 @@
 #include <iostream>
 
-
 class       Harl
 {
     private:
-
     void        debug();
     void        info();
     void        warning();
     void        error();
-
     public:
-    void        complain(std::string level);
+    void        complain(std::string        level);
 };
 
-void        Harl::complain(std::string      level)
+void        Harl::debug()
 {
-    std::string     levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    std::cout << levels[3];
+    std::cout << "Debug meesage" << "\n";
 }
 
-int main()
+void        Harl::info()
 {
-    Harl        c1;
+    std::cout << "Info message" << "\n";
+}
 
+void        Harl::warning()
+{
+    std::cout  << "Warning message" << "\n";
+}
+void    Harl::error()
+{
+    std::cout << "Error message" << "\n";
+}
+
+// return_type (ClassName::*pointer_name)(argument_types) = &ClassName::member_function;
+
+void        Harl::complain(std::string level)
+{
+    std::string     levels[] = {"DUBUG", "INFO", "WARNING", "ERROR"};
+    void        (Harl::*pointer[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (levels[i] == level)
+        {
+            (this->*pointer[i])();
+            return;
+        }
+    }
+    std::cout << "Unknow level\n";
+}
+
+int main(void)
+{
+    Harl   c1;
+    c1.complain("DUBUG");
 }
--- Basic_C++_Programs/cpp01_learning_concepts/tests/p1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-
-int main()
-{
-    int x = 1;
-    int *ptr = &x;
-}
