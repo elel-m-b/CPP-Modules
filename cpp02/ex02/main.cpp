5098d693ef5ed08d076fbffd814d59048122b057
El Hassane El M'Barki <mbkhassan10@gmail.com>
2025-12-31 16:31:00 +0100

    clean cpp02

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,13 +6,13 @@ int main()
     Fixed a;
     Fixed const b(Fixed(5.05f) * Fixed(2));
 
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << Fixed::max(a, b) << std::endl;
+    std::cout << "value of a == " << a << std::endl;
+    std::cout << "value of ++a == " << ++a << std::endl;
+    std::cout << "value of a == " << a << std::endl;
+    std::cout << "value of a++ == " << a++ << std::endl;
+    std::cout << "value of a == " << a << std::endl;
+    std::cout << "value of b == " << b << std::endl;
+    std::cout << "value of max == " << Fixed::max(a, b) << std::endl;
 
     return 0;
 }
