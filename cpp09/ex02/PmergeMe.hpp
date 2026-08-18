5b504e5dc4257aabca8fdca0adad5d982d813bb3
elel-m-b <mbkhassan10@gmail.com>
2026-08-18 03:19:56 +0100

    Finish cpp09/ex02 implimentation

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -1,20 +1,30 @@
-#pragma once
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <utility>
 #include <climits>
 #include <cstdlib>
-#include <iostream>
-#include <cstdlib>
-#include <climits>
 #include <cctype>
 #include <stdexcept>
+#include <ctime>
 
 class PmergeMe
 {
     private:
     std::vector<int> _vector;
     std::deque<int> _deque;
+
+    // Vector helpers
+    void fordJohnsonVector(std::vector<int>& v);
+    std::vector<size_t> generateJacobsthalOrder(size_t size);
+    size_t binarySearchVector(const std::vector<int>& chain,int value,size_t end);
+
+    // Deque helpers
+    void fordJohnsonDeque(std::deque<int>& d);
+    size_t binarySearchDeque(const std::deque<int>& chain,int value,size_t end);
 
     public:
     PmergeMe();
@@ -26,6 +36,8 @@ class PmergeMe
     const std::vector<int>& getVector() const;
     const std::deque<int>& getDeque() const;
 
-    void createVectorPairs();
-    void createDequePairs();
+    void sortVector(std::vector<int>& v);
+    void sortDeque(std::deque<int>& d);
 };
+
+#endif
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -3,37 +3,159 @@
 template <typename Container>
 void printContainer(const Container& container)
 {
-    for (typename Container::const_iterator it = container.begin();it != container.end();++it){
+    typename Container::const_iterator it;
+
+    for (it = container.begin(); it != container.end(); ++it)
         std::cout << *it << " ";
-    }
+
     std::cout << std::endl;
 }
 
-int PmergeMe::parseNumber(const std::string& str){
-    if (str.empty())
-        throw std::runtime_error("Invalid number");
-    for (size_t i = 0; i < str.length(); i++){
-        if (!std::isdigit(str[i]))
-            throw std::runtime_error("Invalid number");
-    }
-    long number = std::strtol(str.c_str(), NULL, 10);
-    if (number > INT_MAX)
-        throw std::runtime_error("Number too large");
-    return static_cast<int>(number);
-}
-
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-    PmergeMe m;
-
-    for (int i = 1; i < ac; i++)
+    if (argc < 2)
     {
-        int number = m.parseNumber(av[i]);
-        m.addNumber(number);
+        std::cerr << "Error" << std::endl;
+        return 1;
     }
 
-    std::cout << "Before: ";
-    printContainer(m.getVector());
-    m.createVectorPairs();
+    try
+    {
+        PmergeMe pmergeMe;
+
+        /*
+        ** ---------------------------------------------------------------
+        ** 1. PARSE ARGUMENTS
+        ** ---------------------------------------------------------------
+        **
+        ** Example:
+        **
+        ** ./PmergeMe 3 5 9 7 4
+        **
+        ** argv:
+        ** argv[1] = "3"
+        ** argv[2] = "5"
+        ** argv[3] = "9"
+        ** argv[4] = "7"
+        ** argv[5] = "4"
+        **
+        ** Each string is converted to an int and added to both:
+        **
+        ** vector
+        ** deque
+        */
+        for (int i = 1; i < argc; ++i)
+        {
+            int number = pmergeMe.parseNumber(argv[i]);
+            pmergeMe.addNumber(number);
+        }
+
+        /*
+        ** ---------------------------------------------------------------
+        ** 2. PRINT BEFORE
+        ** ---------------------------------------------------------------
+        */
+
+        std::cout << "Before: ";
+
+        printContainer(pmergeMe.getVector());
+
+        /*
+        ** ---------------------------------------------------------------
+        ** 3. COPY THE CONTAINERS
+        ** ---------------------------------------------------------------
+        **
+        ** getVector() and getDeque() return const references.
+        **
+        ** We make copies because sortVector() and sortDeque()
+        ** modify the containers.
+        */
+        std::vector<int> vector = pmergeMe.getVector();
+        std::deque<int> deque = pmergeMe.getDeque();
+
+        /*
+        ** ---------------------------------------------------------------
+        ** 4. SORT VECTOR + MEASURE TIME
+        ** ---------------------------------------------------------------
+        */
+
+        std::clock_t startVector = std::clock();
+
+        pmergeMe.sortVector(vector);
+
+        std::clock_t endVector = std::clock();
+
+        /*
+        ** ---------------------------------------------------------------
+        ** 5. SORT DEQUE + MEASURE TIME
+        ** ---------------------------------------------------------------
+        */
+
+        std::clock_t startDeque = std::clock();
+
+        pmergeMe.sortDeque(deque);
+
+        std::clock_t endDeque = std::clock();
+
+        /*
+        ** ---------------------------------------------------------------
+        ** 6. PRINT AFTER
+        ** ---------------------------------------------------------------
+        */
+
+        std::cout << "After:  ";
+
+        printContainer(vector);
+
+        /*
+        ** ---------------------------------------------------------------
+        ** 7. CALCULATE TIME
+        ** ---------------------------------------------------------------
+        **
+        ** std::clock() returns processor clock ticks.
+        **
+        ** CLOCKS_PER_SEC tells us how many ticks correspond to
+        ** one second.
+        **
+        ** Multiplying by 1,000,000 converts seconds to microseconds.
+        */
+        double vectorTime =
+            static_cast<double>(endVector - startVector)
+            / CLOCKS_PER_SEC
+            * 1000000.0;
+
+        double dequeTime =
+            static_cast<double>(endDeque - startDeque)
+            / CLOCKS_PER_SEC
+            * 1000000.0;
+
+        /*
+        ** ---------------------------------------------------------------
+        ** 8. PRINT TIMINGS
+        ** ---------------------------------------------------------------
+        */
+
+        std::cout
+            << "Time to process a range of "
+            << vector.size()
+            << " elements with std::vector : "
+            << vectorTime
+            << " us"
+            << std::endl;
+
+        std::cout
+            << "Time to process a range of "
+            << deque.size()
+            << " elements with std::deque  : "
+            << dequeTime
+            << " us"
+            << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
